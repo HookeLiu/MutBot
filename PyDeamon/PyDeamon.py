1b42aa8fb07cd6a41fd73bb9cb8ba2486350ac42
佚之狗 <Hooke@yhhtech.com>
2019-12-27 19:27:47 +0800

    改成了只打开win32event来避免时序问题

--- PyDeamon/PyDeamon.py
+++ PyDeamon/PyDeamon.py
@@ -1,6 +1,5 @@
 # 中间层: 配置和控制前端和后端
 import win32event 
-import win32ui, win32con
 import os
 import sys
 import time, datetime
@@ -22,7 +21,23 @@ AdminQQ = "2154055060" # 打算做成用配置文件的, 以后再说吧...
 Flag_exit = False
 
 logging.info("--------程序启动--------")
-logging.debug("程序启动, 开始寻找数据库文件")
+logging.debug("程序启动, 开始监听前端事件")
+# 先创建和打开用到的win32event
+TriggerEvent = "Global\\CQdbSync" 
+UpdatedEvent = "Global\\CQupdate" 
+ExitEvent    = "Global\\CQexit"   
+
+try:
+    CQTigger  = win32event.OpenEvent(win32event.EVENT_ALL_ACCESS , True , TriggerEvent )
+    CQupdate  = win32event.OpenEvent(win32event.EVENT_ALL_ACCESS , True , UpdatedEvent )
+    CQexit    = win32event.OpenEvent(win32event.EVENT_ALL_ACCESS , True , ExitEvent )
+    logging.debug("win32event打开成功, 程序开始监听")
+except:
+    logging.error("win32event打开失败, 可能是前端未正确运行.")
+    logging.info("**********程序退出************")
+    sys.exit(-233)
+
+win32event.SetEvent(CQupdate) # 程序启动后给前端一个反馈以便前端判断后端状态
 
 # 因为目前的设计是整个框架都使用数据库传递和记录数据, 所以这个中间层程序需要打开两个数据库: 前端app的db和酷Q自己的eventv2.db
 # 不确定主程序和这个脚本在不在同一个目录, 又想要让各个模块间独立性尽可能强, 所以就搞成找文件的了... 也许这里以后得重新设计...
@@ -42,30 +57,7 @@ for path, folder, files in os.walk(Path_script_root):
                 logging.error("找到了过多的文件, 请检查运行环境")
                 break
         
-Conn_APP = sqlite3.connect(PATH_DB[-2])
-Coon_CQ  = sqlite3.connect(PATH_DB[-1])
-Curs_APP = Conn_APP.cursor()
-Curs_CQ  = Coon_CQ.cursor()
-
 logging.debug( "找到了%d个db文件, 取最后两个( %s, %s ), 耗时%.3f秒" %(count_files, PATH_DB[-2], PATH_DB[-1], time.time() - Time_start ) )
-
-# 先创建和打开用到的win32event
-TriggerEvent = "Global\\CQdbSync" 
-UpdatedEvent = "Global\\CQupdate" 
-ExitEvent    = "Global\\CQexit"   
-
-CQtrigger = win32event.CreateEvent(None, False, False, TriggerEvent )
-logging.info("已创建酷Q触发事件, 为了正确监听酷Q事件, 需动启动酷Q")
-win32ui.MessageBox("目前程序需要手动启动酷Q并启用APP插件, 请手动操作后再点确定.", "需要手动操作", win32con.MB_OK )
-time.sleep(10)
-try:
-    CQupdate  = win32event.OpenEvent(win32event.EVENT_ALL_ACCESS , True , UpdatedEvent )
-    CQexit    = win32event.OpenEvent(win32event.EVENT_ALL_ACCESS , True , ExitEvent )
-    logging.debug("win32event打开成功, 程序开始监听")
-except:
-    logging.error("win32event打开失败")
-    win32ui.MessageBox("win32event打开失败, 请检查运行环境以及酷Q运行情况并重新运行", "出现错误", win32con.MB_OK )
-    sys.exit(-233)
         
 # 用于触发酷Q的事件就在操作完成时SetEvent, 另外两个则需要开启两个线程等待酷Q触发
 
@@ -79,7 +71,7 @@ def Gexit():
             logging.info("收到退出信号, 程序准备结束并退出")
             Flag_exit = True
             pass # 这里放程序退出逻辑
-    return
+    return 0
 
 # 酷Q发出更新信号时说明有需要立即处理的事件, 中间层需要做预处理或移交给后端
 def Update():
@@ -89,6 +81,19 @@ def Update():
         waitFlag = win32event.WaitForSingleObject( CQupdate, 60000 )
         if waitFlag == win32event.WAIT_OBJECT_0 :
             logging.info("收到更新信号, 开始后台处理...")
+            try:
+                Conn_APP = sqlite3.connect(PATH_DB[-2])
+                Coon_CQ  = sqlite3.connect(PATH_DB[-1])
+                Curs_APP = Conn_APP.cursor()
+                Curs_CQ  = Coon_CQ.cursor()
+            except OperationalError :
+                logging.error("数据库打开失败")
+                logging.info("**********程序退出************")
+                sys.exit(-233)
+            except NameError :
+                logging.error("数据库游标创建失败")
+                logging.info("**********程序退出************")
+                sys.exit(-233)
             tasks = getNewTasks()
             if tasks == 0:
                 return
@@ -97,6 +102,10 @@ def Update():
                 query_insert = "INSERT INTO `main`.`event` (`TYPE`, `CONT`, `STATUS`) VALUES (5233, '" + cmd + "', 233);"
                 Curs_APP.execute(query_insert)
             pass # 这里写要做的处理
+            Curs_APP = None
+            Conn_APP.close()
+            Curs_CQ = None
+            Coon_CQ.close()
     return
 
 
@@ -116,6 +125,7 @@ Thread_waitForTigg = threading.Thread(target = Update, args = ())
 Thread_waitForExit.start()
 Thread_waitForTigg.start()
 
+Thread_waitForTigg.join()
 Thread_waitForExit.join()
-
+logging.info("**********程序退出************")
 sys.exit(0)
