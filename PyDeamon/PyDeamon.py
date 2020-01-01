80ac75263c0751820322694f98c6ad52b00a8cda
HookeLiu <Hooke@yhhtech.com>
2020-01-01 15:19:36 +0800

    修了修BUG

--- PyDeamon/PyDeamon.py
+++ PyDeamon/PyDeamon.py
@@ -6,13 +6,27 @@ import time, datetime
 import threading
 import sqlite3
 
+os.system("title 中间层服务")
+
 import logging
 logFile = "./debug.log"
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
-                    datefmt='%a, %d %b %Y %H:%M:%S',
-                    filename=logFile,
-                    filemode='a+')
+logFormat =  logging.Formatter('[%(levelname)s] | %(asctime)s @%(relativeCreated)d | "%(filename)s", line %(lineno)d, in `%(threadName)s` : %(message)s', '%d %b %Y %H:%M:%S')
+printFormat = logging.Formatter('%(asctime)s : %(message)s')
+fh = logging.FileHandler(logFile)
+fh.setFormatter(logFormat)
+ch = logging.StreamHandler() 
+ch.setFormatter(printFormat)
+# ch.setLevel(logging.INFO)
+ch.setLevel(logging.DEBUG)
+
+logger0 = logging.getLogger("all") # 根记录器把所有内容记录到文件
+logger0.setLevel(logging.DEBUG)
+logger0.addHandler(fh) 
+
+logger1 = logging.getLogger("all.consleLog") # 控制台记录器的过滤器只显示info以上的, 所有记录会交到上层
+logger1.addHandler(ch) 
+
+logger1.info("--------程序开始运行--------")
 
 import simpleTest # 打算以后做成读配置文件自动导入后端应用, 就像酷Q那样
 
@@ -20,24 +34,28 @@ AdminQQ = "2154055060" # 打算做成用配置文件的, 以后再说吧...
 
 Flag_exit = False
 
-logging.info("--------程序启动--------")
-logging.debug("程序启动, 开始监听前端事件")
+logger1.debug("应用初始化完成, 准备创建通信事件")
 # 先创建和打开用到的win32event
 TriggerEvent = "Global\\CQdbSync" 
 UpdatedEvent = "Global\\CQupdate" 
 ExitEvent    = "Global\\CQexit"   
 
 try:
-    CQTigger  = win32event.OpenEvent(win32event.EVENT_ALL_ACCESS , True , TriggerEvent )
-    CQupdate  = win32event.OpenEvent(win32event.EVENT_ALL_ACCESS , True , UpdatedEvent )
-    CQexit    = win32event.OpenEvent(win32event.EVENT_ALL_ACCESS , True , ExitEvent )
-    logging.debug("win32event打开成功, 程序开始监听")
+    CQTigger  = win32event.CreateEvent(None, False, False, TriggerEvent )
+    CQupdate  = win32event.CreateEvent(None, False, False, UpdatedEvent )
+    CQexit    = win32event.CreateEvent(None, False, False, ExitEvent )
+    logger1.debug("win32event创建成功, 程序开始通信")
 except:
-    logging.error("win32event打开失败, 可能是前端未正确运行.")
-    logging.info("**********程序退出************")
+    logger1.error("win32event打开失败, 可能是前端未正确运行.")
+    logger1.info("**********程序退出************")
     sys.exit(-233)
 
-win32event.SetEvent(CQupdate) # 程序启动后给前端一个反馈以便前端判断后端状态gitgit 
+# 初始化一下, 避免之前残留触发的影响
+win32event.ResetEvent(CQexit)
+win32event.ResetEvent(CQupdate)
+win32event.ResetEvent(CQTigger)
+
+win32event.SetEvent(CQupdate) # 程序启动后给前端一个反馈以便前端判断后端状态
 
 # 因为目前的设计是整个框架都使用数据库传递和记录数据, 所以这个中间层程序需要打开两个数据库: 前端app的db和酷Q自己的eventv2.db
 # 不确定主程序和这个脚本在不在同一个目录, 又想要让各个模块间独立性尽可能强, 所以就搞成找文件的了... 也许这里以后得重新设计...
@@ -54,10 +72,16 @@ for path, folder, files in os.walk(Path_script_root):
             PATH_DB.append(path)
             count_files += 1
             if count_files > 5:
-                logging.error("找到了过多的文件, 请检查运行环境")
+                logger1.error("找到了过多的文件, 请检查运行环境")
                 break
+
+if count_files < 2 :
+    logger1.critical("找不到数据库! 请确保运行环境正常. 如果是初次使用, 请尝试先用前端创建数据表.")
+    logger1.info("**********程序退出************")
+    sys.exit(-233)
+
         
-logging.debug( "找到了%d个db文件, 取最后两个( %s, %s ), 耗时%.3f秒" %(count_files, PATH_DB[-2], PATH_DB[-1], time.time() - Time_start ) )
+logger1.debug( "找到了%d个db文件, 取最后两个( %s, %s ), 耗时%.3f秒" %(count_files, PATH_DB[-2], PATH_DB[-1], time.time() - Time_start ) )
         
 # 用于触发酷Q的事件就在操作完成时SetEvent, 另外两个则需要开启两个线程等待酷Q触发
 
@@ -68,7 +92,7 @@ def Gexit():
     while waitFlag != win32event.WAIT_OBJECT_0: 
         waitFlag = win32event.WaitForSingleObject( CQexit, 60000 ) # 虽然需要它一直等待, 但设置超时能防止无法退出, 超时了再重新等待就好了
         if waitFlag == win32event.WAIT_OBJECT_0 :
-            logging.info("收到退出信号, 程序准备结束并退出")
+            logger1.info("收到退出信号, 程序准备结束并退出")
             Flag_exit = True
             pass # 这里放程序退出逻辑
     return 0
@@ -78,47 +102,63 @@ def Update():
     global Flag_exit
     waitFlag = 233
     while Flag_exit != True: 
-        waitFlag = win32event.WaitForSingleObject( CQupdate, 60000 )
+        waitFlag = win32event.WaitForSingleObject( CQupdate, 1000 )
         if waitFlag == win32event.WAIT_OBJECT_0 :
-            logging.info("收到更新信号, 开始后台处理...")
+            logger1.info("收到更新信号, 开始后台处理...")
             try:
-                Conn_APP = sqlite3.connect(PATH_DB[-2])
-                Coon_CQ  = sqlite3.connect(PATH_DB[-1])
+                Conn_APP = sqlite3.connect(PATH_DB[-1])
+                Coon_CQ  = sqlite3.connect(PATH_DB[-2])
                 Curs_APP = Conn_APP.cursor()
                 Curs_CQ  = Coon_CQ.cursor()
             except OperationalError :
-                logging.error("数据库打开失败")
-                logging.info("**********程序退出************")
+                logger1.error("数据库打开失败")
+                logger1.info("**********程序退出************")
                 sys.exit(-233)
             except NameError :
-                logging.error("数据库游标创建失败")
-                logging.info("**********程序退出************")
+                logger1.error("数据库游标创建失败")
+                logger1.info("**********程序退出************")
                 sys.exit(-233)
-            tasks = getNewTasks()
+            tasks = getNewTasks(Curs_CQ)
             if tasks == 0:
                 return
-            for EID, LINK, CONT, STATUS in tasks:
-                cmd = simpleTest.应用选择器(STATUS, LINK, EID)
-                query_insert = "INSERT INTO `main`.`event` (`TYPE`, `CONT`, `STATUS`) VALUES (5233, '" + cmd + "', 233);"
-                Curs_APP.execute(query_insert)
-            pass # 这里写要做的处理
+            else :
+                logger1.info("找到%s个任务, 开始处理..." % len(tasks))
+                count_cmd = 0
+                for EID, LINK, CONT, STATUS in tasks:
+                    logger1.debug("任务的事件ID→%s; 对应链接→%s; 内容→%s; 状态号→%s" %(EID, LINK, CONT, STATUS) )
+                    cmd = simpleTest.应用选择器(Curs_APP, Curs_CQ, STATUS, LINK, EID, CONT)
+                    if cmd != None:
+                        query_insert = "INSERT INTO `main`.`event` (`TYPE`, `CONT`, `STATUS`) VALUES (5233, '" + cmd + "', 233);"
+                        Curs_APP.execute(query_insert)
+                        logger1.debug("中间层返回了命令→%s" %(cmd) )
+                        count_cmd += 1
+                pass # 这里写要做的处理
+            logger1.info("任务处理结束, 继续睡觉...")
+            Coon_APP.commit()
+            if count_cmd > 0:
+                logger1.debug("置CQTigger通知前端取命令...")
+                win32event.SetEvent(CQTigger)
             Curs_APP = None
             Conn_APP.close()
+            Coon_CQ.commit()
             Curs_CQ = None
             Coon_CQ.close()
+            win32event.ResetEvent(CQupdate)
     return
 
-
-
-def getNewTasks():
-    query_selectTasks = "SELECT `EID`, `LINK`, `CONT`, `STATUS` FROM `main`.`event` WHERE `STATUS` > 233 ORDER BY `STATUS` DESC;"
+def getNewTasks(Curs_APP):
+    query_selectTasks = "SELECT `EID`, `LINK`, `CONT`, `STATUS` FROM `main`.`event` WHERE `STATUS` > 266 ORDER BY `STATUS` DESC;"
     curs = Curs_APP.execute(query_selectTasks)
     reslut = curs.fetchall()
     rows = len(reslut)
     if rows > 0 :
         return reslut
     return 0
-          
+     
+logger1.debug("为了方便通知前端后端程序在线, 先睡30秒")
+time.sleep(30)
+win32event.ResetEvent(CQupdate)
+logger1.debug("开始创建退出检测线程和任务处理线程")
 Thread_waitForExit = threading.Thread(target = Gexit, args = ())
 Thread_waitForTigg = threading.Thread(target = Update, args = ())
 
@@ -127,5 +167,5 @@ Thread_waitForTigg.start()
 
 Thread_waitForTigg.join()
 Thread_waitForExit.join()
-logging.info("**********程序退出************")
+logger1.info("**********程序退出************")
 sys.exit(0)
--- PyDeamon/simpleTest.py
+++ PyDeamon/simpleTest.py
@@ -1,4 +1,5 @@
 # 只是一个随意的示例...
+import time
 
 词库 = {
     "名词":{
@@ -15,41 +16,50 @@
     "龙狼去蹲坑了", "龙狼蹲坑回来了", "龙狼去厕所了", "龙狼在蹲坑吗"
 ]
 
-def 应用选择器(STATUS, LINK):
-    query_oriGrp = "SELECT `group` FROM `main`.`event` WHERE `id` = " + str(LINK) + ";"
-    curs = Curs_CQ.execute(query_oriGrp)
-    oriGrp = curs.fetchone()[0][9:]
-    query_oriMSG = "SELECT `content` FROM `main`.`event` WHERE `id` = " + str(LINK) + ";"
-    curs = Curs_CQ.execute(query_oriMSG)
-    oriMSG = curs.fetchone()[0]
+def 应用选择器(Curs_APP, Curs_CQ, STATUS, LINK, EID, CONT):
+    cmd = ""
+    if LINK > 0:
+        query_oriGrp = "SELECT `group` FROM `main`.`event` WHERE `id` = " + str(LINK) + ";"
+        curs = Curs_CQ.execute(query_oriGrp)
+        oriGrp = curs.fetchone()[0][9:]
+        query_oriMSG = "SELECT `content` FROM `main`.`event` WHERE `id` = " + str(LINK) + ";"
+        curs = Curs_CQ.execute(query_oriMSG)
+        oriMSG = curs.fetchone()[0]
 
     if STATUS == 401:
         cmd = "send msg to" + AdminQQ + "?(" + str(LINK) + ")\n" + CONT + "?"
         query_update = "UPDATE `main`.`event` SET `CONT`= '" + cmd + "', `STATUS` = 233 WHERE `EID` = " + str(EID) + ";"
-        curs = Curs_CQ.execute(query_update)
+        Curs_APP.execute(query_update)
 
     if STATUS == 302:
         pass # 暂时没条件做那么多了, 先放着咕咕咕...
     if STATUS == 301:
         pass
     if STATUS == 300:
-        if oriMSG.find("龙狼蹲坑去了"):
-            cmd = 蹲坑计时(0)
+        if CONT.find("龙狼蹲坑去了") > 0:
+            cmd = 蹲坑计时(0, oriGrp)
             query_insert = "INSERT INTO `main`.`event` (`TYPE`, `CONT`, `STATUS`) VALUES (5234, '龙狼去蹲坑了', 266);"
             Curs_APP.execute(query_insert)
+            Coon_APP.commit()
             return cmd
-        if oriMSG.find("龙狼蹲坑回来了"):
+        if CONT.find("龙狼蹲坑回来了") > 0:
             query_select = "SELECT `EID`, `TIME`, `CONT`, `STATUS` FROM `main`.`event` WHERE `STATUS` = 266 AND `CONT` = '龙狼去蹲坑了' ;"
             curs = Curs_APP.execute(query_select)
             recordTime = curs.fetchone()[1]
-            timeArray = time.strptime(recordTime, "%Y-%m-%d %H:%M:%S")
-            timeStamp = int(time.mktime(timeArray)) + 8 * 3600
+            tmp_timeArray = time.strptime(recordTime, "%Y-%m-%d %H:%M:%S")
+            recordTimeStamp = int(time.mktime(timeArray)) + 8 * 3600
+            duration = time.time() - recordTimeStamp;
+            cmd = 蹲坑计时(duration, oriGrp)
         pass
+    if len(cmd) > 1:
+        return cmd
+    else :
+        return None
 
 
 
-def 蹲坑计时(sta):
-    if sta < 1:
+def 蹲坑计时(dur, oriGrp):
+    if dur < 1:
         cmd = "send grp to %s ?开始计时, 现在时间是 %s ?" % ( oriGrp, time.strftime("%H:%M:%S", time.localtime())  )
     else :
         cmd = "send grp to %s ?计时结束, 蹲了 %s 秒?" % ( oriGrp, sta )
