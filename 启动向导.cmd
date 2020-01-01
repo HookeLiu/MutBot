611c50abde79c44e0a7236387f45c279d4141045
HookeLiu <Hooke@yhhtech.com>
2020-01-01 19:57:51 +0800

    修改了使用说明

--- 启动向导.cmd
+++ 启动向导.cmd
@@ -9,8 +9,9 @@ echo 本项目目前分为两个较为独立部分, 前端使用酷Q作为用户接口, 后端为可扩展的应用
 :STA
 echo   ---------------------------
 echo 现在准备运行中间层脚本, 请确保python3环境正常并且已经正确安装pywin32包...
-set /p input=准备好后输入Y继续：
-if "%input%"=="Y"(goto RUNscript) else (goto STA)
+echo.
+set /p input=准备好后输入大写的Y以继续：
+if "%input%"=="Y" (goto RUNscript) else (goto STA)
 
 :RUNscript
 echo 正在启动脚本...
@@ -22,7 +23,6 @@ set /p input=是否弹出了新窗口并提示程序开始运行?(Y/N)：
 if "%input%"=="Y" (goto OKToexit) else if "%input%"=="N" (goto ErrToexit) else (goto confm)
 
 :OKToexit
-echo.
 echo   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo 中间层的详细日志保存在debug.log里
 echo 接下来请手动启动酷Q并确保前端APP已正常运行, 如果一切正常就可以按任意键关闭此窗口了~
@@ -30,8 +30,7 @@ TIMEOUT /T 30
 exit
 
 :ErrToexit
-echo.
 echo   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo 那么再检查运行环境重试吧...程序现在退出...
+echo 那么再检查一下运行环境重试吧...现在开始执行退出命令...
 python -c "import win32event; CQexit = win32event.CreateEvent(None, False, False, 'Global\\CQexit'); win32event.SetEvent(CQexit); win32event.SetEvent(CQexit);"
 TIMEOUT /T 10
